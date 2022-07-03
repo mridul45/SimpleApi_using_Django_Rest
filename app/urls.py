4523847467d636658ab000987782b59fffceb6fc
Mridul Singhal <73380291+mridul45@users.noreply.github.com>
2022-07-03 21:31:50 +0530

    Add files via upload

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,18 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
+from rest_framework.routers import DefaultRouter
+
+
+router = DefaultRouter()
+router.register(r"todo-view-set",views.TodoViewSet, basename='todo')
 
 urlpatterns = [
     path('',views.home,name='home'),
-    path('post-todo/',views.post_todo,name='post-todo'),
+    path('get-todo/',views.get_todo,name='get_todo'),
+    path('post-todo/',views.post_todo,name='post_todo'),
+    path('todo/',views.TodoView.as_view()),
 ]
+
+
+urlpatterns += router.urls
--- app/views.py
+++ app/views.py
@@ -2,6 +2,12 @@ from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializer import *
+from rest_framework.views import APIView
+from rest_framework import status,viewsets
+from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticated
+
+
 
 # Create your views here.
 @api_view(['GET','POST','PATCH']) # --> here we need to specify the methods we wat to apply ot if we try to test POST method error will we thrown.
@@ -35,6 +41,18 @@ def home(request):
 
 # the above is a get api
 # The api_view decorator converts the django function into an api
+
+
+@api_view(['GET'])
+def get_todo(request):
+    todo_objs = Todo.objects.all()
+    serializer = TodoSerializer(todo_objs,many=True)
+
+    return Response({
+        'status':True,
+        'message':'Data Fetched',
+        'data':serializer.data
+    })
 
 
 @api_view(['POST'])
@@ -71,3 +89,119 @@ def post_todo(request):
             'status':False,
             'message':'Something went wrong',
         })
+
+
+@api_view(['PATCH'])
+def patch_todo(request):
+    try:
+        data = request.data
+        if not data.get('uid'):
+            return Response({
+                'status':False,
+                'message':'UID is required',
+                'data':{}
+            })
+
+        obj = Todo.objects.get(uid=data.get('uid'))
+        serializer = TodoSerializer(obj,data=data,partial=True)
+        if serializer.is_valid():
+            return Response({
+            'status':True,
+            'message':'Success Operation',
+            'data':serializer.data
+        })
+
+        return Response({
+            'status':False,
+            'message':'invalid data',
+            'data':serializer.errors
+        })
+
+    except Exception as e:
+        print(e)
+        return Response({
+            'status':False,
+            'message':'invalid uid',
+            'data':{}
+        })
+
+
+# Lets Learn about API views
+
+class TodoView(APIView):
+    permission_classes = [IsAuthenticated]
+    def get(self,request):
+        todo_objs = Todo.objects.filter(user=request.user)
+        serializer = TodoSerializer(todo_objs,many=True)
+
+        return Response({
+            'status':True,
+            'message':'Data Fetched',
+            'data':serializer.data
+        })
+
+    def post(self,request):
+        try:
+            data = request.data
+            data['user'] = request.user.id
+            serializer = TodoSerializer(data=data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response({
+                'status':True,
+                'message':'Success Operation',
+                'data':serializer.data
+            })
+            
+            
+            
+            return Response({
+                'status':False,
+                'message':'invalid data',
+                'data':serializer.errors
+            })
+
+            print(data)
+            return Response({
+                'status':True,
+                'message':'Success todo created',
+            })
+
+        except Exception as e:
+            print(e)
+            return Response({
+                    'status':False,
+                    'message':'Something went wrong',
+                }) 
+
+
+class TodoViewSet(viewsets.ModelViewSet):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
+
+
+    @action(detail=False, methods=['post'])
+    def add_date_to_todo(self,request):
+        try:
+            data = request.data
+            serializer = TimingTodoSerializer(data=data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response({
+                'status':True,
+                'message':'Success Operation',
+                'data':serializer.data
+            })
+
+            return Response({
+                    'status':False,
+                    'message':'invalid data',
+                    'data':serializer.errors
+                })
+
+        except Exception as e:
+            print(e)
+            return Response({
+                    'status':False,
+                    'message':'Something went wrong',
+                }) 
